dd5aaa15e423c82ec93ddd376663b2647c6cac70
Aikhan Zhashkeyev <spy.04@mail.ru>
2024-07-18 01:54:07 +0500

    finish mobile css and html

--- source/js/app.js
+++ source/js/app.js
@@ -1,1 +1,6 @@
-console.log('hii')
+const navButton = document.querySelector('.nav__button');
+const navListClass = document.querySelector('.nav__list').classList;
+
+navButton.addEventListener('click', () => {
+  navListClass.toggle('nav__list--active');
+});
