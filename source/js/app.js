ba878399e983f85a3d49d88c0a3a9a3a4ad3ee45
Aikhan Zhashkeyev <spy.04@mail.ru>
2024-07-26 16:10:16 +0500

    finish css and html in index tablet

--- source/js/app.js
+++ source/js/app.js
@@ -2,5 +2,6 @@ const navButton = document.querySelector('.nav__button');
 const navListClass = document.querySelector('.nav__list').classList;
 
 navButton.addEventListener('click', () => {
+  navButton.classList.toggle('nav__button--active');
   navListClass.toggle('nav__list--active');
 });
